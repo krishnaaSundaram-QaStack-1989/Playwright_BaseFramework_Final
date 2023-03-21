dc0f1229d1951c628cca540426a9c97a8cf9d74e
krishnasundaramTalentInternational <krishna.sundaram@talentinternational.com>
2023-03-21 09:46:31 +0530

    Config , env , ts config added

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,90 +1,46 @@
-import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
-export default defineConfig({
-  testDir: './tests',
-  /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
-  expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
-    timeout: 5000
-  },
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+import type { PlaywrightTestConfig } from "@playwright/test";
+import devices from "@playwright/test";
+import { testInputs } from "@testDatas/testData";
+require("dotenv").config();
+const config: PlaywrightTestConfig = {
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    actionTimeout: 15000,
+    screenshot: "on",
   },
-
-  /* Configure projects for major browsers */
+  testDir: "./src/tests/",
+  timeout: 60 * 1000,
+  expect: {
+    timeout: testInputs.waitTime.mediumWaits,
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: 2,
+  reportSlowTests: { max: 10, threshold: 60000 },
+  repeatEach: 1,
+  workers: process.env.CI ? 4 : undefined,
+  reporter: [['line'], ['allure-playwright']],
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "smokeTests",
+      testDir: "src/tests/smokeTests",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
     },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "regressionTests",
+      testDir: "src/tests/regressionTests",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
     },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "New-flows",
+      testDir: "src/tests/regressionTests/tests/",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { channel: 'chrome' },
-    // },
   ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
-});
+};
+export default config;
